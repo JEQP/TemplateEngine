e259dc3833ccdd6fb9e21b365eed734edb0112b2
James Pearce <jamesquintanapearce@gmail.com>
2020-01-24 22:08:35 +1100

    added psuedocode in cli.js

---
+++ cli.js
@@ -0,0 +1,51 @@
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const inq = require("inquirer");
+
+// set a const empty array
+
+// Prompt user to create an Engineering Team. Add Manager, input Manager Name
+
+// inq Manager ID
+
+// inq Manager Email
+
+// inq Manager officeNumber
+
+// add to array
+
+//// Loop starts here
+
+// inq would you like to add an employee?
+
+// if yes
+
+// inq which employee would you like to add? MENU
+
+//// if Engineer 
+
+//// inq Engineer name
+
+//// inq Engineer ID
+
+//// inq Engineer Email
+
+//// inq Engineer Github
+
+//// else if Intern
+
+//// inq Intern name
+
+//// inq Intern ID
+
+//// inq Intern email
+
+//// inq Intern School
+
+//// add response to array (possibly via a second const array)
+
+//// Loop ends here
+
+// if no move to creating HTML
